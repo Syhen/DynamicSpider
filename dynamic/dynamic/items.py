26a90b0552cfa7eb26f43297dd05bbe3ed2eb6ed
mac <heyao@dreamy.live>
2017-04-08 17:37:54 +0800

    dynamic item class.

--- dynamic/dynamic/items.py
+++ dynamic/dynamic/items.py
@@ -5,10 +5,31 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+import six as _six
+from scrapy.item import DictItem as _DictItem, ItemMeta as _ItemMeta
 
 
-class DynamicItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class _DynamicItem(_DictItem):
+    """make a class of `scrapy.Item`"""
+
+    def __init__(self, item_names):
+        for name in item_names:
+            self.fields[name] = None
+        super(_DynamicItem, self).__init__()
+
+    def __call__(self):
+        return self
+
+
+@_six.add_metaclass(_ItemMeta)
+class ItemMaker(_DynamicItem):
+    pass
+
+
+if __name__ == '__main__':
+    ItemClass = ItemMaker(['name', 'value', 'url'])
+    item = ItemClass()
+    item['name'] = 'heyao'
+    item['value'] = 22
+    item['url'] = 'https://github.com/syhen'
     pass
