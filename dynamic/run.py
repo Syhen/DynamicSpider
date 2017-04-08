d36efbf8daa6795b41bfbc03dc77cde7ee050738
mac <heyao@dreamy.live>
2017-04-08 17:45:53 +0800

    run dynamic spider.

---
+++ dynamic/run.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2017/04/08 17:42:40
+
+@author: mac
+"""
+
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+from dynamic.spiders.DynamicSpider import DynamicSpider
+
+import pymongo
+
+
+if __name__ == '__main__':
+    con = pymongo.MongoClient()
+    db = con.spider
+    ids = [8]
+    rules = db.rules.find({'_id': {'$in': ids}})
+    con.close()
+
+    settings = get_project_settings()
+    runner = CrawlerRunner(settings)
+    configure_logging(settings)
+
+    for rule in rules:
+        runner.crawl(DynamicSpider, rule=rule)
+
+    d = runner.join()
+    d.addBoth(lambda _: reactor.stop())
+
+    reactor.run()
