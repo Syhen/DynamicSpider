f83ad3c087ee4c2c0a985951a2f2e6f1e17e41d3
mac <heyao@dreamy.live>
2017-04-10 17:34:15 +0800

    update spider: add extract data from scrapy.Item; add string join(prefix, mark)

--- dynamic/dynamic/spiders/DynamicSpider.py
+++ dynamic/dynamic/spiders/DynamicSpider.py
@@ -5,6 +5,7 @@ Created on 2017/04/08 17:38:48
 @author: mac
 """
 
+import re
 import scrapy
 from dynamic.items import ItemMaker
 
@@ -27,15 +28,38 @@ class DynamicSpider(scrapy.Spider):
             )
 
     def parse(self, response):
-        print response.url
+        """
+        all spider parse will go through the way as:
+        1.crawl data from page;(use xpath or css)
+        2.get info from crawled messages;(use regulation)
+        3.join messages;(str + str)
+        :param response: response object.
+        :return: yield item or Request object.
+        """
         body_list = response.xpath(self.rule['body_list'])
         for body in body_list:
             item = self.DynamicItem()
             meta = self.rule['meta']
             item.update(meta)
+            # xpath extract
             for name in self.rule['xpath']:
                 item['_'.join(name.split('_')[:-1])] = body.xpath(self.rule['xpath'][name]).extract()[0]
-            print item
+
+            # get message extract
+            for key in self.rule['re']:
+                reg = self.rule['re'][key]['reg']
+                string = item[self.rule['re'][key]['str_name']]
+                reg = re.compile(str('%s' % reg))  # translate u'' -> r''
+                result = reg.search(string)
+                item[key] = result and result.group(0) or ''
+
+            # join
+            for key in self.rule['join']:
+                join_dict = self.rule['join'][key]
+                item[key] = join_dict['prefix'] + join_dict['join_str'] + item[join_dict['end_name']]
+
+            yield item
+
         next_page = response.xpath(self.rule['next_page'])
         if next_page:
             yield scrapy.Request(
