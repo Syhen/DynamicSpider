d497c12e26ee2b0f450455cd7ecf936f295f4df3
mac <heyao@dreamy.live>
2017-04-13 15:46:38 +0800

    add run DynamicJSSpider test

--- dynamic/run.py
+++ dynamic/run.py
@@ -10,6 +10,7 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 from dynamic.spiders.DynamicSpider import DynamicSpider
+from dynamic.spiders.DynamicJSSpider import DynamicJSSpider
 
 import pymongo
 
@@ -27,6 +28,8 @@ if __name__ == '__main__':
 
     for rule in rules:
         runner.crawl(DynamicSpider, rule=rule)
+    # for rule in rules:
+    #     runner.crawl(DynamicJSSpider, rule=rule)
 
     d = runner.join()
     d.addBoth(lambda _: reactor.stop())
